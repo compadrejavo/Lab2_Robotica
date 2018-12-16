b638da17d3abfe1741f5c0174c87af628f07cc2b
Javo <javier.araya@usach.cl>
2018-12-15 22:29:18 -0400

    intento de balanceo
    se va de hocico

--- Robot_Balanceador/Robot_Balanceador.ino
+++ Robot_Balanceador/Robot_Balanceador.ino
@@ -1,15 +1,24 @@
+
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Wire.h"
 
 char estado = '0';  //variable que recibe las instrucciones del celular
 
-int ENA = 6; // MCU PWM Pin 10 to ENA on L298n Board
-int IN1 = 7;  // MCU Digital Pin 9 to IN1 on L298n Board 
-int IN2 = 8;  // MCU Digital Pin 8 to IN2 on L298n Board 
-int IN3 = 9;  // MCU Digital pin 7 to IN3 on L298n Board
-int IN4 = 10;  // MCU Digital pin 6 to IN4 on L298n Board
-int ENB = 11;  // MCU PWM Pin 5 to ENB on L298n Board
+//Pines puente H
+int ENA = 10; // MCU PWM Pin 10 to ENA on L298n Board
+int IN1 = 9;  // MCU Digital Pin 9 to IN1 on L298n Board 
+int IN2 = 8;  // MCU Digital Pin 8 to IN2 on L298n Board
+ 
+int ENB = 5;  // MCU PWM Pin 5 to ENB on L298n Board
+int IN3 = 7;  // MCU Digital pin 7 to IN3 on L298n Board
+int IN4 = 6;  // MCU Digital pin 6 to IN4 on L298n Board
+
+//Pines giroscopo
+//GND - GND
+//VCC - VCC
+//SDA - Pin A4
+//SCL - Pin A5
 
 const int mpuAddress = 0x68;  // Puede ser 0x68 o 0x69
 MPU6050 mpu(mpuAddress);
@@ -21,6 +30,128 @@ long tiempo_prev;
 float dt;
 float ang_x, ang_y;
 float ang_x_prev, ang_y_prev;
+
+void setup() 
+{
+  Serial.begin(9600);    
+  pinMode(ENA, OUTPUT); //Set all the L298n Pin to output
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  Wire.begin();
+  mpu.initialize();
+}
+
+void loop() {
+  estado='x'; // Resetear variable
+  if(Serial.available()>0){  // Entrar solo cuando se envíe algo
+    estado=Serial.read();  // Leer lo que la antena reciba   
+    
+    switch(estado){ 
+      case 'f':  forward();     break;
+      case 'b':  backward();   break;
+      case 'l':  left();     break;
+      case 'r':  right();    break;
+      case 'h':  halt();    break;   
+      default: halt();     
+    }//Fin switch  
+  }
+  delay(10); 
+  estabilizar();
+}
+
+
+//------------------------------------------ Movimiento Motores DC---------------------------
+void forward(){  
+  digitalWrite(IN1, HIGH); 
+  digitalWrite(IN2, LOW);  
+  digitalWrite(IN3, HIGH); 
+  digitalWrite(IN4, LOW);  
+  analogWrite(ENA, 200); 
+  analogWrite(ENB, 200);
+  delay(200); 
+}
+
+void right(){  
+  digitalWrite(IN1, HIGH); 
+  digitalWrite(IN2, LOW);  
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, HIGH);  
+  analogWrite(ENA, 200); 
+  analogWrite(ENB, 200);
+  delay(200);
+}
+
+void backward(){   
+  digitalWrite(IN1, LOW); 
+  digitalWrite(IN2, HIGH);  
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH); 
+  analogWrite(ENA, 200); 
+  analogWrite(ENB, 200);
+  delay(200); 
+}
+
+void left (){ 
+  digitalWrite(IN1, LOW); 
+  digitalWrite(IN2, HIGH);  
+  digitalWrite(IN3, HIGH); 
+  digitalWrite(IN4, LOW);  
+  analogWrite(ENA, 200); 
+  analogWrite(ENB, 200);
+  delay(200);
+}
+
+void halt(){ 
+  digitalWrite(IN1, LOW); // Apagar Motor
+  digitalWrite(IN2, LOW); // Apagar Motor
+  digitalWrite(IN3, LOW); // Apagar Motor
+  digitalWrite(IN4, LOW); // Apagar Motor 
+}
+
+//------------------------------------------ Estabilizar ---------------------------
+
+void estabilizar(){   
+  analogWrite(ENA, 250); 
+  analogWrite(ENB, 250);
+  if(angulox()<3 ){
+    digitalWrite(IN1, LOW); 
+    digitalWrite(IN2, HIGH);  
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+  else if(angulox()>3 ){
+    digitalWrite(IN1, HIGH); 
+    digitalWrite(IN2, LOW);  
+    digitalWrite(IN3, HIGH); 
+    digitalWrite(IN4, LOW);
+  } 
+  else{
+    digitalWrite(IN1, LOW); // Apagar Motor
+    digitalWrite(IN2, LOW); // Apagar Motor
+    digitalWrite(IN3, LOW); // Apagar Motor
+    digitalWrite(IN4, LOW); // Apagar Motor
+  }    
+}
+
+//------------------------------------------ Sensor Inercial ---------------------------
+
+int angulox()
+{
+  mpu.getAcceleration(&ax, &ay, &az);
+  mpu.getRotation(&gx, &gy, &gz);
+  updateFiltered();  
+  delay(10);  
+  // Mostrar resultados
+   Serial.print(F("Rotacion en X:  "));
+   Serial.print(ang_x);
+   Serial.print(F("\t Rotacion en Y: "));
+   Serial.println(ang_y);
+  return ang_x;
+}
 
 void updateFiltered()
 {
@@ -37,88 +168,4 @@ void updateFiltered()
  
    ang_x_prev = ang_x;
    ang_y_prev = ang_y;
-}
-
-void setup() 
-{
-  Serial.begin(9600);    
-  pinMode(ENA, OUTPUT); //Set all the L298n Pin to output
-  pinMode(ENB, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-
-  Wire.begin();
-  mpu.initialize();
-}
-int angulox()
-{
-  mpu.getAcceleration(&ax, &ay, &az);
-  mpu.getRotation(&gx, &gy, &gz);
-  updateFiltered();
-  return ang_x;
-}
-void loop() {
-  estado='x'; // Resetear variable
-  if(Serial.available()>0){  // Entrar solo cuando se envíe algo
-    estado=Serial.read();  // Leer lo que la antena reciba   
-    
-    switch(estado){ 
-      case 'f':  forward();     break;
-      case 'b':  backward();   break;
-      case 'l':  left();     break;
-      case 'r':  right();    break;
-      case 'h':  halt();    break;        
-    }//Fin switch  
-  }
-  delay(100);
-}
-
-
-
-//------------------------------------------ Movimiento Motores DC---------------------------
-void forward(){  
-  digitalWrite(IN1, HIGH); 
-  digitalWrite(IN2, LOW);
-  analogWrite(ENA, 255);  // velocidad 0 a 255
-  digitalWrite(IN3, HIGH); 
-  digitalWrite(IN4, LOW);  
-  analogWrite(ENB, 255); // velocidad 0 a 255
-}
-
-void right(){  
-  analogWrite(ENA, 255); // velocidad 0 a 255
-  analogWrite(ENB, 255);
-  digitalWrite(IN1, LOW); 
-  digitalWrite(IN2, HIGH);  
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW); 
-  delay(10);
-}
-
-void backward(){   
-  analogWrite(ENA, 255); //Rueda izquierda
-  analogWrite(ENB, 255); //Rueda derecha
-  digitalWrite(IN1, LOW); 
-  digitalWrite(IN2, HIGH);  
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);   
-}
-
-void left (){ 
-  analogWrite(ENA, 255); 
-  analogWrite(ENB, 255);
-  digitalWrite(IN1, HIGH); 
-  digitalWrite(IN2, LOW);  
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH); 
-  delay(10);
-}
-
-void halt(){ 
-  digitalWrite(IN1, LOW); // Apagar Motor
-  digitalWrite(IN2, LOW); // Apagar Motor
-  digitalWrite(IN3, LOW); // Apagar Motor
-  digitalWrite(IN4, LOW); // Apagar Motor
 }
